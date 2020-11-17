55a00fbcd89f6b3d1bd187309c5cf003d4b3bc55
Keelhauled <keelhauledhs@gmail.com>
2020-11-18 01:01:14 +0200

    Use unique target name

--- packaging/GenerateTarget.ps1
+++ packaging/GenerateTarget.ps1
@@ -1,5 +1,6 @@
 param([string]$id, [string]$savePath)
 
 $xml = [xml](Get-Content ".\template.targets")
+$xml.Project.Target.Name = $id.Replace('.', '')
 $xml.Project.Target.ItemGroup.ReferenceCopyLocalPaths.Condition = "'%(ReferenceCopyLocalPaths.NuGetPackageId)' == '$($id)'"
 $xml.Save([IO.Path]::Combine($savePath, $id + ".targets"))
