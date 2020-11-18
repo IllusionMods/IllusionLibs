feb1fd41a2ae881baa34d4a07f938734e3642998
Keelhauled <keelhauledhs@gmail.com>
2020-11-18 22:30:35 +0200

    Fix versions

--- packaging/pack.ps1
+++ packaging/pack.ps1
@@ -9,22 +9,22 @@ function GenerateTarget {
 }
 
 function Pack {
-    param([string]$game, [string]$gameVer, [string]$unityVer)
+    param([string]$game, [string]$gameVersion, [string]$unityVersion)
 
     gci -Path $game -Filter *.nuspec | foreach {
         $id = "IllusionLibs.$($game).$($_.BaseName)"
         GenerateTarget $id $_.DirectoryName
-        nuget pack $_.FullName -properties "id=$($id);gameVersion=$($gameVer);unityVersion=$($unityVer)";
+        nuget pack $_.FullName -properties "id=$($id);gameVersion=$($gameVersion);unityVersion=$($unityVersion)";
     }
 }
 
 Pack "AIGirl" "2020.05.29.2" "2018.2.21.3"
-Pack "AIShoujo" "2019.12.20.2" "5.5.5.2"
-Pack "EmotionCreators" "2019.6.6.2" "5.5.5.2"
-Pack "Koikatu" "2019.4.27.2" "5.5.5.2"
-Pack "KoikatsuParty" "2019.4.27.2" "5.5.5.2"
+Pack "AIShoujo" "2019.12.20.2" "2018.2.21.3"
+Pack "EmotionCreators" "2019.6.6.2" "2017.4.24.3"
+Pack "Koikatu" "2019.4.27.2" "5.6.2.2"
+Pack "KoikatsuParty" "2019.4.27.2" "5.6.2.2"
 Pack "PlayHome" "2018.3.11.2" "5.5.5.2"
-Pack "HoneySelect" "2017.6.30.2" "5.5.5.2"
-Pack "HoneySelect2" "2020.05.29.2" "5.5.5.2"
+Pack "HoneySelect" "2017.6.30.2" "5.3.5.2"
+Pack "HoneySelect2" "2020.05.29.2" "2018.4.11.2"
 Pack "XUnity" "" ""
 Pack "BepInEx" "" ""
