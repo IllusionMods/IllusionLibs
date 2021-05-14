0100d9e2605d18e26ce22e4a29762c7b4b9499cf
ManlyMarco <dummyaccx@gmail.com>
2021-05-14 20:37:44 +0200

    Set projects to allow unsafe when packages are installed

--- packaging/install.ps1
+++ packaging/install.ps1
@@ -7,3 +7,12 @@ foreach ($reference in $project.Object.References)
         $reference.CopyLocal = $false;
     }
 }
+
+# Set the "Allow unsafe code" flag in all project configurations
+# Needed to allow the code calling publicized assemblies to work
+for($i = 1; $i -le $project.ConfigurationManager.Count; $i++)  
+{ 
+    $project.ConfigurationManager.Item($i).Properties.Item("AllowUnsafeBlocks").Value = "True"
+}
+# Save to make sure the change doesn't get lost
+$project.Save()
