1c7dc4cd8ad7ffa146873b7b0642eb7e4011fe49
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-10-05 11:07:40 +0200

    Add strip-only script

--- mono-cil-strip/strip-folder.bat
+++ mono-cil-strip/strip-folder.bat
@@ -1,13 +1,5 @@
 @echo off
-
-rem Needed for older versions AssemblyPublicizer to find other dlls before I fixed it
 cd /d %~1
-
-rem Need to do this before stripping or it dies horribly
-%~dp0\AssemblyPublicizer.exe --quiet %~1
-rem Replace original files with the output
-move %~1\publicized_assemblies\*.dll %~1
-rmdir %~1\publicized_assemblies
 
 FOR /R %~1 %%i IN (*.dll) DO (
 %~dp0\mono-cil-strip.exe %%i
